1e21483ff9023b2237e88a6f827fde7a57ca2e95
catuchi <atuchibueze@gmail.com>
2024-07-21 14:56:35 -0400

    arithmetic

---
+++ arithmetic.php
@@ -0,0 +1,33 @@
+<?php
+    // Arithmetic operators
+    // + - * / ** %
+    $x = 10;
+    $y = 2;
+    $z = null;
+
+    // $z = $x + $y;
+    // $z = $x - $y;
+    // $z = $x * $y;
+    // $z = $x / $y;
+    // $z = $x ** $y;
+    // $z = $x % $y;
+    echo $z;
+
+    // Increment/Decrement operators
+    $counter = 10;
+
+    // $counter = $counter + 1;
+    // $counter++;
+    // $counter--;
+    // $counter+=2;
+    // $counter-=2;
+    // echo $counter;
+
+    // Operator Precedence
+    // ()
+    // **
+    // * / %
+    // + -
+    $total = 1 + 2 - 3 * 4 / 5 ** 6;
+    echo $total;
+?>
